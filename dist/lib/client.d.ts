76af3590b8cbd4c63d4c3e6dad6c34426476cb00
Ange <93579219+Ange230700@users.noreply.github.com>
2025-08-10 13:15:56 +0200

    Delete dist directory

--- dist/lib/client.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "nihildbpost/prisma/generated/client";
-declare const prisma: PrismaClient<import("nihildbpost/prisma/generated/client").Prisma.PrismaClientOptions, never, import("nihildbpost/generated/client/runtime/library").DefaultArgs>;
-export default prisma;
