73e267e1276bb3d48265ede4b04f7669d9893a3b
Ange <93579219+Ange230700@users.noreply.github.com>
2025-08-10 18:12:44 +0200

    Delete dist directory

--- dist/seed.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const NUM_POSTS: number;
-export declare function seedPosts(skipCleanup?: boolean): Promise<void>;
-export default function seed(skipCleanup?: boolean): Promise<void>;
